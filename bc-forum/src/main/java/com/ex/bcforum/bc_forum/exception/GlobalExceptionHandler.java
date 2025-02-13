f726747254bf44830d70fa47f8d94d9abceff364
Jack.Choi <choisk0999@gmail.com>
2025-02-13 14:55:18 +0800

    update

--- bc-forum/src/main/java/com/ex/bcforum/bc_forum/exception/GlobalExceptionHandler.java
+++ bc-forum/src/main/java/com/ex/bcforum/bc_forum/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.ex.bcforum.bc_forum.exception;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 
 import com.ex.bcforum.bc_forum.codewave.ApiResp;
 import com.ex.bcforum.bc_forum.codewave.BusinessException;
@@ -17,6 +18,11 @@ public class GlobalExceptionHandler {
   @ExceptionHandler(value = IllegalArgumentException.class)
   public ApiResp handleIllegalArgException(IllegalArgumentException e){
     return ApiResp.of(SysCode.INVALID_INPUT);
+  }
+
+  @ExceptionHandler(value = HttpClientErrorException.class)
+  public ApiResp handleHttpClientErrorException(HttpClientErrorException e){
+    return ApiResp.of(SysCode.RESTTEMPLATE_ERROR);
   }
 
 }
